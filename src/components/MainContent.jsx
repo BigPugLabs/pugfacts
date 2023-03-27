53807eff43aa743f4bfb6eaceadf00617ea1b878
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-27 03:31:37 +0100

    pugfacts v1

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import logo from "../assets/react.svg"
+
+export function MainContent() {
+    return (
+        <section>
+            <h2>Top Pug Facts</h2>
+            <ul>
+                <li>Pugs are snuggly</li>
+                <li>Pugs are big</li>
+                <li>Pug puppies are smol</li>
+                <li>Pugs will stare at food</li>
+                <li>Pugs can climb on top of logs</li>
+                <li>Pugs sleep all day</li>
+            </ul>
+        </section>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import logo from "../assets/react.svg"
+
+export function Navbar() {
+    return (
+        <nav>
+            <img src={logo} />
+            <h3>PugFacts</h3>
+            <h4>BigPug Data</h4>
+        </nav>
+    )
+}
